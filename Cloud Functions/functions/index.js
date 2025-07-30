746729a66b0e98d6c1fca18204d65c5f70fc5fcb
fred <ey24984@bristol.ac.uk>
2025-07-30 16:49:02 +0100

    inital build out of cloud func

--- Cloud Functions/functions/index.js
+++ Cloud Functions/functions/index.js
@@ -1,19 +1,50 @@
 /**
  * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+ **/
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onObjectFinalized } = require("firebase-functions/v2/storage"); // new upload
+const { initializeApp } = require("firebase-admin/app");
+const { getStorage } = require("firebase-admin/storage");
 const logger = require("firebase-functions/logger");
+const path = require("path");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+data.append("media", fs.createReadStream("/full/path/to/image.jpg"));
+data.append("models", "type,text,genai");
+data.append("api_user", "620259281");
+data.append("api_secret", "xBjw7YPmAM63MVPZJFaMydCQDRZL8Ho3");
+initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//Images bucket = easel-e5104.firebasestorage.app, location USCENTRAL1
+// Set location to align with location of cloud storage. For some reason the database is in eur3, there would have been a reason why FB recommended or enforced using US for cloud...
+// Docs say that Function may be in any location if the trigger region is set to us-central1 so could set to eur3 to match firestore but we'll see
+
+exports.checkImage = onObjectFinalized(
+  {
+    region: "us-central1",
+    bucket: "easel-e5104.firebasestorage.app",
+  },
+  (event) => {
+    data = new FormData();
+
+    axios({
+      method: "post",
+      url: "https://api.sightengine.com/1.0/check.json",
+      data: data,
+      headers: data.getHeaders(),
+    })
+      .then(function (response) {
+        // on success: handle response
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        if (error.response) console.log(error.response.data);
+        else console.log(error.message);
+      });
+
+    console.log("item uploaded");
+  }
+);
